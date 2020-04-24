6ba93411e227f8453d94c8ab6a35c23b3f569c3d
RapDog <denis152325@yandex.ru>
2020-04-24 21:01:30 +0300

    implement logic in the client class.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import com.company.entity.Admin;
+import com.company.entity.User;
+
 public class Main {
     public static void main(String[] args) {
-	    System.out.println("Hello, world!");
-        System.out.println("Hello, world!");
+        User vassol = new User("Vassol", "Petrov", 25);
+        User igor = new User("Igor", "Ivanov", 35);
+
+        Admin admin = new Admin();
+        admin.makePersonAdmin(igor);
+        admin.makePersonModerator(vassol);
     }
 }

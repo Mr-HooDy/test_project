c919573b4e55278489996c03f0a3cc7a31594a85
RapDog <denis152325@yandex.ru>
2020-04-24 19:18:06 +0300

    Add the make moderator method for admin

---
+++ src/com/company/entity/Admin.java
@@ -0,0 +1,13 @@
+package com.company.entity;
+
+/**
+ * @author RapDog
+ * @created by 4/24/2020
+ * @version 1.0
+ */
+public class Admin {
+
+    public void makeModerator(){
+        System.out.println("Making a person  moderator");
+    }
+}

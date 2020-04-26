1aa7a0f1f1d3ef9210b93e8144184d2af1bbc938
RapDog <denis152325@yandex.ru>
2020-04-26 22:05:53 +0300

    Making conflict

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,7 @@
 package com.company;
 
-import com.company.entity.Admin;
-import com.company.entity.User;
-
-import com.company.entity_vlad.last_class;
-
 public class Main {
     public static void main(String[] args) {
-        User vassol = new User("Vassol", "Petrov", 25);
-        User igor = new User("Igor", "Ivanov", 35);
-
-        Admin admin = new Admin();
-        admin.makePersonAdmin(igor);
-        admin.makePersonModerator(vassol);
-        last_class l = new last_class();
-	    System.out.println("H");
+        System.out.println("Conflict");
     }
 }

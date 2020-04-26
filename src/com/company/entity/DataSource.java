e995a9becdb04e972a320278b967bfcfc0207268
RapDog <denis152325@yandex.ru>
2020-04-26 21:59:16 +0300

    Added data source entity.

---
+++ src/com/company/entity/DataSource.java
@@ -0,0 +1,23 @@
+package com.company.entity;
+
+/**
+ * @author RapDog
+ * @version 1.0
+ * @created by 4/26/2020
+ */
+public class DataSource {
+
+    private String link;
+
+    public DataSource(String link) {
+        this.link = link;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
